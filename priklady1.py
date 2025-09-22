248ca0865cb1a5991df477a600b69d91fea15a70
Adela Bierska <adela.bierska@gmail.com>
2025-09-22 19:11:42 +0200

    Task 03

--- priklady1.py
+++ priklady1.py
@@ -17,4 +17,24 @@ sales = {
 sales["Noc, která mě zabila"] = 0
 sales["Vrah zavolá v deset"] += 100
 
-print(sales)
+# print(sales)
+
+# Task 03
+tombola = {
+    7: "Láhev kvalitního vína Château Headache",
+    15: "Pytel brambor z místního družstva",
+    23: "Čokoládový dort",
+    47: "Kniha o historii města",
+    55: "Šiška salámu",
+    67: "Vyhlídkový let balónem",
+    79: "Moderní televizor",
+    91: "Roční předplatné městského zpravodaje",
+    93: "Společenská hra Sázky a dostihy",
+}
+
+number = int(input("Zadej sve cislo: "))
+
+if number in tombola:
+    print(f"Vyhral*a jsi! Tvou cenou je: {tombola[number]}")
+else:
+    print("Bohuzel nevyhravas nic.")
