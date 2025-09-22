5f52b41f92a69acc7ded71682816228fe23001b9
Adela Bierska <adela.bierska@gmail.com>
2025-09-22 19:05:55 +0200

    Task 02

--- priklady1.py
+++ priklady1.py
@@ -1,9 +1,20 @@
 # Task 01
-
 school_report = {
     "cesky jazyk": 2,
     "matematika": 1,
     "dejepis": 4
 }
 
-print(school_report)
+# print(school_report)
+
+# Task 02
+sales = {
+    "Zkus mě chytit": 4165,
+    "Vrah zavolá v deset": 5681,
+    "Zločinný steh": 2565,
+}
+
+sales["Noc, která mě zabila"] = 0
+sales["Vrah zavolá v deset"] += 100
+
+print(sales)
