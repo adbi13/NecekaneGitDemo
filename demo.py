bf454c097dbed2b0d2903743d71728f812d9c4af
Adela Bierska <adela.bierska@gmail.com>
2025-09-22 20:10:33 +0200

    Add lists of dicts

--- demo.py
+++ demo.py
@@ -61,25 +61,46 @@ empty_dict = dict()
 
 
 # -------------------------- Slovniky a cykly
-sales = {
-    "Zkus mě chytit": 4165,
-    "Vrah zavolá v deset": 5681,
-    "Zločinný steh": 2565,
-}
+# sales = {
+#     "Zkus mě chytit": 4165,
+#     "Vrah zavolá v deset": 5681,
+#     "Zločinný steh": 2565,
+# }
 
-# Klice = keys
-for houba in sales.keys():
-    print(houba)
+# # Klice = keys
+# for houba in sales.keys():
+#     print(houba)
 
-# =
+# # =
 
-# for key in sales:
-#     print(key)
+# # for key in sales:
+# #     print(key)
 
-# Hodnoty = values
-for hrib in sales.values():
-    print(hrib)
+# # Hodnoty = values
+# for hrib in sales.values():
+#     print(hrib)
 
-#   = items
-for lysohlavka, muchomurka in sales.items():
-    print(f"Klic: {lysohlavka}, Hodnota: {muchomurka}")
+# #   = items
+# for lysohlavka, muchomurka in sales.items():
+#     print(f"Klic: {lysohlavka}, Hodnota: {muchomurka}")
+
+# print(sum(sales.values()))
+
+books = [ # seznam (list)
+    {"title": "Zkus mě chytit", "sold": 4165, "price": 347, "year": 2018},
+    {"title": "Vrah zavolá v deset", "sold": 5681, "price": 299, "year": 2019},
+    {"title": "Zločinný steh", "sold": 2565, "price": 369, "year": 2019},
+]
+
+book = books[0] # slovnik (dict)
+        # { "title": "Zkus mě chytit", "sold": 4165, "price": 347, "year": 2018 }
+
+sold_qty = book["sold"] # cislo (int)
+
+
+total_sales = 0
+for item in books:
+    total_sales += item["sold"]
+print(f"Celkem bylo prodáno {total_sales} knih.")
+
+
