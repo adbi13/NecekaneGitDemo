a4cce8feb84d605fff42b4347c4b63cce11d9685
Adela Bierska <adela.bierska@gmail.com>
2025-09-22 18:16:58 +0200

    Finalize FE

---
+++ demo.py
@@ -0,0 +1,11 @@
+stock = [
+    ["JBL", "XYZ 123", 1500, 40],
+    ["Sony", "gggg 89", 2000, 13],
+    ["Beats", "chachacha", 3500, 0]
+]
+
+
+
+for item in stock:
+    print(f"Brand: {item[0]}\tModel: {item[1]}\tPrice: {item[2]}\tStock Qty: {item[3]}")
+
