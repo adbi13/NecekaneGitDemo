a73e733a0bd6da51994d48a4a412c1b9f111c2af
Adela Bierska <adela.bierska@gmail.com>
2025-09-22 20:57:05 +0200

    Set 2 Task 02

--- priklady2.py
+++ priklady2.py
@@ -11,9 +11,34 @@ school_report = {
     "Chemie": 4,
 }
 
-prumer = sum(school_report.values())/ len(school_report.values())
-print(f"Průměr známek: {prumer}")
+# prumer = sum(school_report.values())/ len(school_report.values())
+# print(f"Průměr známek: {prumer}")
 
-for predmet, znamka in school_report.items():
-    if znamka == 1:
-        print(predmet)
+# for predmet, znamka in school_report.items():
+#     if znamka == 1:
+#         print(predmet)
+
+# Task 02
+books = [
+    {"title": "Vražda s příliš mnoha notami", "pages": 450, "rating": 5},
+    {"title": "Vražda podle knihy", "pages": 524, "rating": 9},
+    {"title": "Past", "pages": 390, "rating": 4},
+    {"title": "Popel popelu", "pages": 411, "rating": 10},
+    {"title": "Noc, která mě zabila", "pages": 159, "rating": 7},
+    {"title": "Vražda, kouř a stíny", "pages": 258, "rating": 6},
+    {"title": "Zločinný steh", "pages": 542, "rating": 8},
+    {"title": "Zkus mě chytit", "pages": 247, "rating": 7},
+    {"title": "Vrah zavolá v deset", "pages": 396, "rating": 6},
+]
+
+total_pages = 0
+good_books_count = 0
+
+for book in books:
+    total_pages += book["pages"]
+
+    if book["rating"] >= 8:
+        good_books_count += 1
+
+
+# print(total_pages, good_books_count)
