9272c54363f742102b35c5314a5f6c4c3448aa62
Adela Bierska <adela.bierska@gmail.com>
2025-09-22 18:23:55 +0200

    Introduce dicts

--- demo.py
+++ demo.py
@@ -4,8 +4,29 @@ stock = [
     ["Beats", "chachacha", 3500, 0]
 ]
 
+stock = [
+    {
+        "brand": "JBL",
+        "model": "XYZ 123",
+        "price": 1500,
+        "qty": 40
+    },
+    {
+        "brand": "Sony",
+        "model": "gggg 89",
+        "price": 2000,
+        "qty": 13
+    },
+    {
+        "brand": "Beats",
+        "model": "chachacha",
+        "price": 3500,
+        "qty": 0
+    }
+]
+
 
 
 for item in stock:
-    print(f"Brand: {item[0]}\tModel: {item[1]}\tPrice: {item[2]}\tStock Qty: {item[3]}")
+    print(f"Brand: {item['brand']}\tModel: {item['model']}\tPrice: {item['price']}\tStock Qty: {item['qty']}")
 
